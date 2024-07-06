22adb5e280324ecc7db4475ce7b6a9782a75fdc9
lsytj0413 <lsytj0413@gmail.com>
2024-07-06 21:50:34 +0800

    feat(*): add chapter 2.2.3

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,57 @@
+
+struct FirstName {
+    value: String,
+}
+
+struct LastName {
+    value: String,
+}
+
+struct Age {
+    value: i32,
+}
+
+struct Pay {
+    value: i32,
+}
+
+macro_rules! generate_get_value {
+    ($struct_type:ident) => {
+        generate_get_value!($struct_type, String);
+    };
+    ($struct_type:ident, $return_type:ident) => {
+        impl $struct_type {
+            pub fn get_value(&self) -> &$return_type {
+                &self.value
+            }
+        }
+    }
+}
+
+generate_get_value!(FirstName);
+generate_get_value!(LastName);
+generate_get_value!(Age, i32);
+generate_get_value!(Pay, i32);
+
+fn calculate_raise(first_name: FirstName, last_name: LastName, age: Age, pay: Pay) -> Pay {
+    if first_name.get_value() == "Sam" {
+        Pay{
+            value: pay.get_value() + 32
+        }
+    } else {
+        pay
+    }
+}
+
 fn main() {
-    let s = "Hello, world!";
-    println!("Hello, world, {}", s);
+    let s = calculate_raise(FirstName{
+        value: "Sam".to_string(),
+    }, LastName{
+        value: "Smith".to_string(),
+    }, Age{
+        value: 32,
+    }, Pay{
+        value: 100,
+    });
+    println!("Hello, world, {}", s.get_value());
 }
