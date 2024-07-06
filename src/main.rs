3bc5412a4e8ab914209b840d15e112afc96a4e87
lsytj0413 <lsytj0413@gmail.com>
2024-07-06 16:48:42 +0800

    feat(*): add chapter 2.1

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,34 @@
+macro_rules! my_vec {
+    () => [
+        Vec::new()
+    ];
+    [make an empty vec] => (
+        Vec::new()
+    );
+    {$x:expr} => {
+        {
+            let mut v = Vec::new();
+            v.push($x);
+            v
+        }
+    };
+    [$($x:expr),+] => (
+        {
+            let mut v = Vec::new();
+            $(
+                v.push($x);
+            )+
+            v
+        }
+    )
+}
+
+
 fn main() {
-    let s = "Hello, world!";
-    println!("Hello, world, {}", s);
+    let empty: Vec<i32> = my_vec![];
+    println!("{:?}", empty);
+    let also_empty: Vec<i32> = my_vec!(make an empty vec);
+    println!("{:?}", also_empty);
+    let three_numbers = my_vec!(1, 2, 3);
+    println!("{:?}", three_numbers);
 }
