e5589b27308f220622c74e26d61ffdfaa2ae7518
lsytj0413 <lsytj0413@gmail.com>
2024-07-06 21:32:15 +0800

    feat(*): add chapter 2.2.1

---
+++ src/greeting.rs
@@ -0,0 +1,19 @@
+pub fn base_greeting_fn(name: &str, greeting: &str) -> String {
+    format!("{}, {}!", greeting, name)
+}
+
+#[macro_export]
+macro_rules! greeting {
+    ($name:literal) => {
+        {
+            // use $crate::greeting::base_greeting_fn;
+            base_greeting_fn($name, "Hello")
+        }
+    };
+    ($name:literal, $greeting:literal) => {
+        {
+            // use $crate::greeting::base_greeting_fn;
+            base_greeting_fn($name, $greeting)
+        }
+    };
+}
--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,11 @@
+use crate::greeting::base_greeting_fn;
+
+// #[macro_use]  // 如果没有这一行，会报错找不到 greeting!
+mod greeting;
+
 fn main() {
-    let s = "Hello, world!";
-    println!("Hello, world, {}", s);
+    let greet = greeting!("Sam", "Heya");
+    println!("{}", greet);
+    let greet_with_default = greeting!("Sam");
+    println!("{}", greet_with_default);
 }
