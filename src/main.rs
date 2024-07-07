f357442bd4bbf5926e25a87e6c378f242689a1b0
lsytj0413 <lsytj0413@gmail.com>
2024-07-07 09:13:41 +0800

    feat(*): add chapter 2.2.4

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,38 @@
+use std::ops::{Add, Sub};
+
+#[derive(Debug)]
+struct Account {
+    money: u32,
+}
+
+impl Account {
+    fn add(&mut self, money: u32) {
+        self.money = self.money.add(money)
+    }
+    fn subtract(&mut self, money: u32) {
+        self.money = self.money.sub(money)
+    }
+}
+
+macro_rules! exchange {
+    (Give $amount:literal to $name:ident) => {
+        $name.add($amount)
+    };
+    (Take $amount:literal from $name:ident) => {
+        $name.subtract($amount)
+    };
+    (Give $amount:literal from $giver:ident to $receiver: ident) => {
+        $giver.subtract($amount);
+        $receiver.add($amount)
+    };
+}
+
 fn main() {
-    let s = "Hello, world!";
-    println!("Hello, world, {}", s);
+    let mut the_poor = Account { money: 0 };
+    let mut the_rich = Account { money: 100 };
+    exchange!(Give 20 to the_poor);
+    exchange!(Take 10 from the_rich);
+    exchange!(Give 30 from the_rich to the_poor);
+
+    println!("Poor: {:?}, rich: {:?}", the_poor, the_rich);
 }
