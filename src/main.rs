058c1541d01ffed5f342ec9f645441d928d91dbf
lsytj0413 <lsytj0413@gmail.com>
2024-07-07 18:08:02 +0800

    feat(*): add chapter 2.2.5

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,29 @@
+fn add_one(n: i32) -> i32 { n + 1 }
+
+fn stringify(n: i32) -> String { n.to_string() }
+
+fn prefix_with(prefix: &str) -> impl Fn(String) -> String + '_ {
+    move |x| format!("{}{}", prefix, x)
+}
+
+fn compose_two<FIRST, SECOND, THIRD, F, G>(f: F, g: G) -> impl Fn(FIRST) -> THIRD
+where
+    F: Fn(FIRST) -> SECOND,
+    G: Fn(SECOND) -> THIRD
+{
+    move |x| g(f(x))
+}
+
+macro_rules! compose {
+    ($last:expr) => { $last };
+    ($head:expr => $($tail:expr)=>+) => {
+        compose_two($head, compose!($($tail)=>+))
+    }
+}
+
 fn main() {
-    let s = "Hello, world!";
-    println!("Hello, world, {}", s);
+    let composed = compose!(
+        add_one => stringify => prefix_with("Result: ")
+    );
+    println!("Hello, world, {}", composed(5));
 }
