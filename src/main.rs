8e678ecb6df3dabba4690b4ef20270b7539962c9
lsytj0413 <lsytj0413@gmail.com>
2024-07-07 18:34:24 +0800

    feat(*): add chapter 2.2.6

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,12 @@
+macro_rules! curry {
+    (|$first_arg:ident $(, $arg:ident )*| $function_body:expr) => {
+        move |$first_arg| $(move |$arg|)* {
+            $function_body
+        }
+    };
+}
+
 fn main() {
-    let s = "Hello, world!";
-    println!("Hello, world, {}", s);
+    let s = curry!(|a, b, c| a + b + c);
+    println!("Hello, world, {}", s(1)(2)(3));
 }
