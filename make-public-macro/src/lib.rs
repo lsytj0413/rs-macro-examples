a5a2f5591907c5a3d651c6710b61fcce8996d9da
lsytj0413 <lsytj0413@gmail.com>
2024-07-13 15:12:56 +0800

    feat(*): add chapter 4.4

--- make-public-macro/src/lib.rs
+++ make-public-macro/src/lib.rs
@@ -1,16 +1,32 @@
 use proc_macro::TokenStream;
-use syn::{parse_macro_input, DeriveInput};
+use syn::{parse_macro_input, Data::Struct, DataStruct, DeriveInput, FieldsNamed, Fields::Named};
 use quote::quote;
 
 #[proc_macro_attribute]
 pub fn public(_attr: TokenStream, item: TokenStream) -> TokenStream {
     let ast = parse_macro_input!(item as DeriveInput);
     let name = ast.ident;
+    let fields = match ast.data {
+        Struct(
+            DataStruct{
+                fields: Named(
+                    FieldsNamed {
+                        ref named, ..
+                    }),..
+            }
+        ) => named,
+        _ => unimplemented!("only works for structs with named fields")
+    };
+    let builder_fields = fields.iter().map(|f|{
+        let name = &f.ident;
+        let ty = &f.ty;
+        // We cann't use #f.ident, because it cannot access a variable's properties
+        quote! {pub #name: #ty}
+    });
 
     let public_version = quote!{
         pub struct #name {
-            pub first: String,
-            pub second: u32,
+            #(#builder_fields,)*
         }
     };
     public_version.into()
