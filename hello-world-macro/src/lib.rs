732245b5ec392e3d37637e310ea70be18890876c
lsytj0413 <lsytj0413@gmail.com>
2024-07-13 09:47:44 +0800

    feat(*): add chapter 3.6 venial

--- hello-world-macro/src/lib.rs
+++ hello-world-macro/src/lib.rs
@@ -1,17 +1,23 @@
 use quote::quote;
-use proc_macro::{TokenStream, TokenTree};
-use syn::{parse_macro_input, DeriveInput};
+use proc_macro::{TokenStream};
+use venial::{parse_declaration, Declaration, Struct, Enum};
 
 #[proc_macro_derive(Hello)]
 pub fn hello(item: TokenStream) -> TokenStream {
-    fn ident_name(item: TokenTree) -> String {
-        match item {
-            TokenTree::Ident(i) => i.to_string(),
-            _ => panic!("no ident")
+    let declaration = parse_declaration(item.into()).unwrap();
+
+    let name = match declaration {
+        Declaration::Struct(Struct { name, ..}) => name,
+        Declaration::Enum(Enum { name, ..}) => name,
+        _ => panic!("only implemented for struct and enum")
+    };
+
+    let add_hello_world = quote! {
+        impl #name {
+            fn hello_world(&self) {
+                println!("Hello, world!")
+            }
         }
-    }
-    let name = ident_name(item.into_iter().nth(1).unwrap());
-    format!("impl {} {{ fn hello_world(&self) {{ println!(\"Hello world\") }} }}",
-        name,
-    ).parse().unwrap()
+    };
+    add_hello_world.into()
 }
