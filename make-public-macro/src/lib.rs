c8309e6d02b54782f1eae7e96e4591e65078a9ac
lsytj0413 <lsytj0413@gmail.com>
2024-07-13 14:55:10 +0800

    feat(*): add chapter 4.3

--- make-public-macro/src/lib.rs
+++ make-public-macro/src/lib.rs
@@ -1,12 +1,17 @@
-// extern crate core;
-
 use proc_macro::TokenStream;
 use syn::{parse_macro_input, DeriveInput};
 use quote::quote;
 
 #[proc_macro_attribute]
 pub fn public(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    let _ast = parse_macro_input!(item as DeriveInput);
-    let public_version = quote!{};
+    let ast = parse_macro_input!(item as DeriveInput);
+    let name = ast.ident;
+
+    let public_version = quote!{
+        pub struct #name {
+            pub first: String,
+            pub second: u32,
+        }
+    };
     public_version.into()
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,15 @@
 use make_public_macro::public;
 
 #[public]
-struct Example {}
+struct Example {
+    first: String,
+    pub second: u32,
+}
 
 fn main() {
-    let s = "Hello, world!";
-    println!("Hello, world, {}", s);
+    let s = Example {
+        first: "example".to_string(),
+        second: 42,
+    };
+    println!("Hello, world, {}, {}", s.first, s.second);
 }
