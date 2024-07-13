a24743a290b6716646366a1e23ef17a4bdc58b92
lsytj0413 <lsytj0413@gmail.com>
2024-07-13 09:43:55 +0800

    feat(*): add chapter 3.6 native format

--- hello-world-macro/src/lib.rs
+++ hello-world-macro/src/lib.rs
@@ -1,18 +1,17 @@
 use quote::quote;
-use proc_macro::TokenStream;
+use proc_macro::{TokenStream, TokenTree};
 use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro_derive(Hello)]
 pub fn hello(item: TokenStream) -> TokenStream {
-    let ast = parse_macro_input!(item as DeriveInput);
-    let name = ast.ident;
-
-    let add_hello_world = quote! {
-        impl #name {
-            fn hello_world(&self) {
-                println!("Hello, world!");
-            }
+    fn ident_name(item: TokenTree) -> String {
+        match item {
+            TokenTree::Ident(i) => i.to_string(),
+            _ => panic!("no ident")
         }
-    };
-    add_hello_world.into()
+    }
+    let name = ident_name(item.into_iter().nth(1).unwrap());
+    format!("impl {} {{ fn hello_world(&self) {{ println!(\"Hello world\") }} }}",
+        name,
+    ).parse().unwrap()
 }
