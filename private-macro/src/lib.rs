91a6b4986f427073764c956148fd776d161c07af
lsytj0413 <lsytj0413@gmail.com>
2024-07-20 22:15:42 +0800

    feat(*): add chapter 5.1.2

--- private-macro/src/lib.rs
+++ private-macro/src/lib.rs
@@ -4,11 +4,14 @@ use syn::{parse_macro_input, DeriveInput};
 
 #[proc_macro]
 pub fn private(item: TokenStream) -> TokenStream {
+    let item_as_stream: quote::__private::TokenStream = item.clone().into();
+
     let ast = parse_macro_input!(item as DeriveInput);
     let name = ast.ident;
 
     quote!{
-        struct #name {}
+        #item_as_stream 
+        
         impl #name {}
     }.into()
 }
