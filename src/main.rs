1e280f38b2df1a9c21c2a9b53cf6754d8b2b2cb4
lsytj0413 <lsytj0413@gmail.com>
2024-07-21 21:30:24 +0800

    feat(*): add chapter 5.3

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,16 @@
+use function_like_compose_macro::compose;
+
+fn add_one(n: i32) -> i32 {
+    n + 1
+}
+
+fn stringify(n: i32) -> String {
+    n.to_string()
+}
+
 fn main() {
-    let s = "Hello, world!";
-    println!("Hello, world, {}", s);
+    let composed = compose!(
+        add_one . add_one . stringify
+    );
+    println!("{:?}", composed(5))
 }
