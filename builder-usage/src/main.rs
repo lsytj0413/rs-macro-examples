1917e1ccadd77ca2a8dd3428d603c87ea82d188d
lsytj0413 <lsytj0413@gmail.com>
2024-08-01 11:23:18 +0800

    feat(*): add chapter 8.1.1

--- builder-usage/src/main.rs
+++ builder-usage/src/main.rs
@@ -76,4 +76,16 @@ mod tests {
 
         Gleipnir::builder().build();
     }
+
+    #[test]
+    fn should_generate_builder_for_struct_with_one_renamed_property() {
+        #[derive(Builder)]
+        struct Gleipnir {
+            #[rename("tops_of")]
+            roots_of: String,
+        }
+
+        let gleipnir = Gleipnir::builder().tops_of("mountains".to_string()).build();
+        assert_eq!(gleipnir.roots_of, "mountains");
+    }
 }
