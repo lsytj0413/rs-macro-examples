5b676c941d2d2b6c47ee4e7c2f067a699f78437e
lsytj0413 <lsytj0413@gmail.com>
2024-08-01 15:37:36 +0800

    feat(*): add chapter 8.2

--- builder-code/src/fields.rs
+++ builder-code/src/fields.rs
@@ -1,10 +1,10 @@
 use core::panic;
 
 use proc_macro2::TokenStream;
-use quote::{quote};
+use quote::{quote, TokenStreamExt};
 use syn::punctuated::Punctuated;
 use syn::token::{Comma};
-use syn::{Field, Ident, LitStr, Meta};
+use syn::{Expr, ExprLit, Field, Ident, Lit, LitStr, Meta, MetaNameValue};
 use syn::Type;
 
 #[allow(dead_code)]
@@ -50,10 +50,10 @@ fn extract_attribute_from_field<'a>(f: &'a Field, name: &'a str) -> Option<&'a s
     f.attrs.iter().find(|&attr| attr.path().is_ident(name))
 }
 
-pub fn builder_methods(fields: &Punctuated<Field, Comma>) -> impl Iterator<Item = TokenStream> + '_ {
+pub fn builder_methods(fields: &Punctuated<Field, Comma>) -> Vec<TokenStream> {
     fields.iter().map(|f| {
         let (field_name, field_type) = get_name_and_type(f);
-        let attr = extract_attribute_from_field(f, "rename")
+        extract_attribute_from_field(f, "rename")
             .map(|a| &a.meta )
             .map(|m| {
                 match m {
@@ -67,29 +67,42 @@ pub fn builder_methods(fields: &Punctuated<Field, Comma>) -> impl Iterator<Item 
                             "expected brackets with name of field, like #[rename(name)]"
                         )
                     },
-                    Meta::NameValue(_) => {
-                        panic!(
-                            "did not expect name + value"
+                    Meta::NameValue(
+                        MetaNameValue {
+                            value: Expr::Lit(
+                                ExprLit{
+                                    lit: Lit::Str(literal_string),
+                                    ..
+                                }
+                            ),
+                            ..
+                        }
+                    ) => {
+                        Ident::new(
+                            &literal_string.value(), 
+                            literal_string.span(),
                         )
+                    },
+                    _ => panic!("expected key and value for rename attribute")
+                }
+            })
+            .map(|attr| {
+                quote! {
+                    pub fn #attr(mut self, input: #field_type) -> Self {
+                        self.#field_name = Some(input);
+                        self
                     }
                 }
-            });
-        if let Some(attr) = attr {
-            return quote! {
-                pub fn #attr(mut self, input: #field_type) -> Self {
-                    self.#field_name = Some(input);
-                    self
+            })
+            .unwrap_or_else(|| {
+                quote! {
+                    pub fn #field_name(mut self, input: #field_type) -> Self {
+                        self.#field_name = Some(input);
+                        self
+                    }
                 }
-            }
-        }
-
-        quote! {
-            pub fn #field_name(mut self, input: #field_type) -> Self {
-                self.#field_name = Some(input);
-                self
-            }
-        }
-    })
+            })
+    }).collect()
 }
 
 pub fn original_struct_setters(fields: &Punctuated<Field, Comma>) -> impl Iterator<Item = TokenStream> + '_ {
--- builder-usage/src/main.rs
+++ builder-usage/src/main.rs
@@ -88,4 +88,16 @@ mod tests {
         let gleipnir = Gleipnir::builder().tops_of("mountains".to_string()).build();
         assert_eq!(gleipnir.roots_of, "mountains");
     }
+
+    #[test]
+    fn should_generate_builder_for_struct_with_one_renamed_namedvalue_property() {
+        #[derive(Builder)]
+        struct Gleipnir {
+            #[rename = "tops_of"]
+            roots_of: String,
+        }
+
+        let gleipnir = Gleipnir::builder().tops_of("mountains".to_string()).build();
+        assert_eq!(gleipnir.roots_of, "mountains");
+    }
 }
